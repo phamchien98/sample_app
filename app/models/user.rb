ac256d84d40f7872019bf034d4ca8d7c559fdd5e
chienpt98 <pham.thi.chien@sun-asterisk.com>
2020-08-27 16:25:58 +0700

    Chapter6-Modeling-user

---
+++ app/models/user.rb
@@ -0,0 +1,23 @@
+class User < ApplicationRecord
+  before_save :email_downcase
+
+  validates :name, presence: true,
+    length: {maximum: Settings.validations.name.max_length}
+
+  validates :email, presence: true,
+    length: {maximum: Settings.validations.email.max_length},
+    format: {with: Settings.validations.email.regex},
+    uniqueness: {case_sensitive: false}
+
+  validates :password, presence: true,
+    length: {minimum: Settings.validations.password.min_length}
+
+  has_secure_password
+
+
+  private
+
+  def email_downcase
+    email.downcase!
+  end
+end
